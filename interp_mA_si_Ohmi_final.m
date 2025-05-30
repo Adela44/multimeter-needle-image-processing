4d81e9af4d371b969a28bcfe9967b3c98452c68a
Adela Codrean <72125200+Adela44@users.noreply.github.com>
2025-05-30 10:58:16 +0300

    Update interp_mA_si_Ohmi_final.m

--- interp_mA_si_Ohmi_final.m
+++ interp_mA_si_Ohmi_final.m
@@ -3,7 +3,7 @@ x = [-50.85, -51, -53.08, -55.93, -61.04, -69.86, -88.03, 83.02, 74.85, 62.80];
 y_Amp = [6, 7, 9, 13, 19, 28, 50, 61, 73, 89];
 z_Ohm = [820, 680, 560, 390, 270, 180, 100, 82, 68, 56];
 
-% Transformări
+% Transformări (vectorul x_final) pentru unghi
 x_final = x;
 for i = 1:length(x)
     if x(i) < 0
@@ -22,20 +22,22 @@ disp(x_final);
 n = 2;
 p = polyfit(x_final, y_Amp, n);
 
-U = 5 %V
+disp('Coeficienții polinomului (grad 2):');
+disp(p);
+
+U = 5; %V
 
 f_Amp = @(x) (0.0024*x^(2) + 0.832*x - 41.72777);
 f_Ohm = @(x) (U/f_Amp(x))*10^3;
 
 %Exemplu pentru valoarea unghiului 53.08
-disp('Valoarea I (Amp): ')
+disp('Valoarea I (mA): ')
 f_Amp(53)
 
 disp('Valoarea R (Ohm): ')
 f_Ohm(53)
 
-disp('Coeficienții polinomului (grad 2):');
-disp(p);
+
 
 % Generăm puncte pentru desen
 x_fit = linspace(min(x_final), max(x_final), 200);
